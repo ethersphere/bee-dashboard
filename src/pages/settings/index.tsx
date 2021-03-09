9b99c3754bb48a9544ccefb465d3f13727acd209
matmertz25 <40722304+matmertz25@users.noreply.github.com>
2021-03-08 21:31:43 -0500

    add ability to change apis in settings page

--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -1,7 +1,25 @@
-import React from 'react'
-import { Paper, Container, TextField, Typography } from '@material-ui/core';
+import React, { useState } from 'react'
+import { Paper, Container, TextField, Typography, Button } from '@material-ui/core';
 
 export default function Settings() {
+
+    const [refreshVisibility, toggleRefreshVisibility] = useState(false)
+    const [host, setHost] = useState('')
+    const [debugHost, setDebugHost] = useState('')
+
+    const handleNewHostConnection = () => {
+        if (host) {
+            sessionStorage.setItem('api_host', host)
+        }
+        if (debugHost) {
+            sessionStorage.setItem('debug_api_host', debugHost)
+        }
+        if (host || debugHost) {
+            toggleRefreshVisibility(!refreshVisibility)
+            window.location.reload();
+        }
+    }
+
     return (
         <div>
             <Container>
@@ -16,9 +34,14 @@ export default function Settings() {
                     placeholder="ex: 127.0.0.0.1:1633"
                     helperText="Enter node host override / port"
                     fullWidth
+                    defaultValue={sessionStorage.getItem('api_host') ? sessionStorage.getItem('api_host') : process.env.REACT_APP_BEE_HOST}
                     margin="normal"
                     InputLabelProps={{
                         shrink: true,
+                    }}
+                    onChange={(e) => {
+                        setHost(e.target.value)
+                        toggleRefreshVisibility(true)
                     }}
                     variant="filled"
                     />
@@ -31,6 +54,11 @@ export default function Settings() {
                     placeholder="ex: 127.0.0.0.1:1635"
                     helperText="Enter node debug host override / port"
                     fullWidth
+                    defaultValue={sessionStorage.getItem('debug_api_host') ? sessionStorage.getItem('debug_api_host') : process.env.REACT_APP_BEE_DEBUG_HOST}
+                    onChange={(e) => {
+                        setDebugHost(e.target.value)
+                        toggleRefreshVisibility(true)
+                    }}
                     margin="normal"
                     InputLabelProps={{
                         shrink: true,
@@ -38,6 +66,11 @@ export default function Settings() {
                     variant="filled"
                     />
                 </Paper>
+                {refreshVisibility ?
+                <div style={{marginTop:'20px'}}>
+                <Button variant='outlined' color='primary' onClick={() => handleNewHostConnection()}>Save</Button>
+                </div> 
+                : null}
             </Container>
         </div>
     )
