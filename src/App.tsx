00771367c2a32eaf599c2d26f49d4a6e492a57f8
matmertz25 <40722304+matmertz25@users.noreply.github.com>
2021-03-11 17:35:15 -0500

    remove WIP wallet connection code

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,6 @@ import { createMuiTheme } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
-import Web3 from 'web3';
-
 import BaseRouter from './routes/routes';
 
 
@@ -64,69 +62,7 @@ const darkTheme = createMuiTheme({
 function App() {
   const [themeMode, toggleThemeMode] = useState('light');
 
-  // const [accounts, setAccounts] = useState([]);
-  // const [loadingAccounts, setLoadingAccounts] = useState(false);
-
-  // const [network, setNetwork] = useState({});
-  // const [loadingNetwork, setLoadingNetwork] = useState(false);
-
-  // async loadWeb3() {
-  //   if(window.ethereum) {
-  //     window.web3 = new Web3(window.ethereum)
-  //     await window.ethereum.enable()
-  //     window.ethereum.on('chainChanged', (_chainId: string) => window.location.reload());
-  //   } else if (window.web3) {
-  //     window.web3 = new Web3(window.web3.currentProvider)
-  //   } else {
-  //     console.log('Non ethereum supported browser detected. Consider installing metamask')
-  //   }
-
-  // }
-
-  // async loadBlockChainData() {
-  //   const web3 = window.web3
-  //   const accounts = await web3.eth.getAccounts()
-  //   setAccounts(accounts[0])
-
-  //   const networkId = await web3.eth.net.getId()
-
-  //   let chainData = await (await fetch('https://chainid.network/chains.json')).json()
-
-  //   let network = 'unknown';
-  //   network = chainData.find(chain => chain.networkId === networkId)
-
-  //   if (networkId === 1) {
-  //     network = 'mainNet';
-  //   } else if (networkId === 3) {
-  //     network = 'ropsten';
-  //   } else if (networkId === 4) {
-  //     network = 'rinkeby';
-  //   } else if (networkId === 42) {
-  //     network = 'kovan';
-  //   } else if (networkId === 5) {
-  //     network = 'goerli';
-  //   }
-  //   setNetwork(network)
-
-  //   // const networkData = Decentragram.networks[networkId]
-  //   // if (networkData) {
-  //   //   const decentragram = web3.eth.Contract(Decentragram.abi, networkData.address)
-  //   //   this.setState({decentragram})
-  //   //   this.setState({ loading: false})
-  //   // } else {
-  //   //   window.alert('Decentragram contract not deployed to detected network.')
-  //   // }
-  // }
-
-  // interface Setup {
-  //   loadWeb3: () => Promise<any>;
-  //   loadBlockchainData: () => Promise<any>;
-  // }
-
   useEffect(() => {
-    // await loadWeb3()
-    // await loadBlockchainData()
-
     let theme = localStorage.getItem('theme')
 
     if (theme) {
