65d5f4c5f1473fa3ce27e604216683c7e3a2a649
matmertz25 <40722304+matmertz25@users.noreply.github.com>
2021-03-15 20:35:37 -0400

    integrate separated ui component

--- src/App.tsx
+++ src/App.tsx
@@ -2,61 +2,19 @@ import React, { useEffect, useState, createContext } from 'react';
 import {BrowserRouter as Router} from 'react-router-dom';
 import './App.css';
 
-import { createMuiTheme } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
 import BaseRouter from './routes/routes';
 
+import { lightTheme, darkTheme } from './theme';
 
 declare global {
   interface Window {
-      ethereum: {};
-      web3: {};
+      ethereum: any;
+      web3: any;
   }
 }
-
-const lightTheme = createMuiTheme({
-  typography: {
-    fontFamily: [
-      'Montserrat',
-      'Nunito',
-      'Roboto',
-      '"Helvetica Neue"',
-      'Arial',
-      'sans-serif'
-    ].join(','),
-  }
-});
-
-const darkTheme = createMuiTheme({
-  palette: {
-    type: "dark",
-    background: {
-      default: '#0d1117', //'#111827',
-      paper: '#161b22', //'#1f2937',
-    },
-    primary: {
-      // light: will be calculated from palette.primary.main,
-      main: '#5e72e4' //'#3f51b5',
-      // dark: will be calculated from palette.primary.main,
-      // contrastText: will be calculated to contrast with palette.primary.main
-    },
-    secondary: {
-      main: '#1f2937',
-    },
-  },
-  typography: {
-    fontFamily: [
-      'Montserrat',
-      'Nunito',
-      'Roboto',
-      '"Helvetica Neue"',
-      'Arial',
-      'sans-serif'
-    ].join(','),
-  }
-});
 
 
 function App() {
@@ -79,7 +37,7 @@ function App() {
 
     return () => window.matchMedia('(prefers-color-scheme: dark)').removeEventListener('change', changeHandle)
   
-  }, []);
+  }, []); 
 
   return (
     <div className="App">
