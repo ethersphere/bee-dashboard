fc1a8cb0a072855896c4c308c5fb0d2148294aa2
matmertz25 <40722304+matmertz25@users.noreply.github.com>
2021-04-01 05:53:32 -0700

    feat: split api status checks (#37)
    * chore: split node and debug api status checks
    
    * chore: update final status check
    
    * chore: resolved compilation issues
    
    Co-authored-by: Vojtech Simetka <vojtech@simetka.cz>

--- src/pages/status/SetupSteps/NodeConnectionCheck.tsx
+++ src/pages/status/SetupSteps/NodeConnectionCheck.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Typography, Accordion, AccordionSummary, AccordionDetails } from '@material-ui/core/';
-import MuiAlert from '@material-ui/lab/Alert';
 import { CheckCircle, Error, ExpandMoreSharp } from '@material-ui/icons/';
 
 import ConnectToHost from '../../../components/ConnectToHost';
@@ -9,7 +8,7 @@ import CodeBlockTabs from '../../../components/CodeBlockTabs'
 export default function NodeConnectionCheck(props: any) {
     return (
         <div>
-      <p>Connect to Bee Node APIs</p>
+        <p>Connect to Bee Node API</p>
           <div style={{display:'flex', marginBottom: '25px'}}>
             { props.nodeApiHealth ? 
               <CheckCircle style={{color:'#32c48d', marginRight: '7px', height: '18px'}} />
@@ -59,67 +58,6 @@ export default function NodeConnectionCheck(props: any) {
             </Typography>
           :
           null}
-          </div>
-          <div>
-            <div style={{display:'flex', marginBottom: '25px'}}>
-              { props.nodeHealth?.status === 'ok' ? 
-                <CheckCircle style={{color:'#32c48d', marginRight: '7px', height: '18px'}} />
-                :
-                <Error style={{color:'#c9201f', marginRight: '7px', height: '18px'}} />
-              }
-              <span style={{marginRight:'15px'}}>Debug API  (<Typography variant="button">{props.debugApiHost}</Typography>)</span>
-              <ConnectToHost hostName={'debug_api_host'} defaultHost={props.debugApiHost} />
-            </div>
-            <div>
-            { props.nodeHealth?.status !== 'ok' ? 
-              <Typography component="div" variant="body2" gutterBottom style={{margin: '15px'}}>
-                We cannot connect to your nodes debug API at <Typography variant="button">{props.debugApiHost}</Typography>. Please check the following to troubleshoot your issue.
-                <Accordion style={{marginTop:'20px'}}>
-                  <AccordionSummary
-                    expandIcon={<ExpandMoreSharp />}
-                    aria-controls="panel1a-content"
-                    id="panel1a-header"
-                  >
-                    <Typography>Troubleshoot</Typography>
-                  </AccordionSummary>
-                  <AccordionDetails>
-                    <Typography component="div">
-                    <ol>
-                      <li>Check the status of your node by running the below command to see if your node is running.</li>
-                      <CodeBlockTabs
-                      showLineNumbers
-                      linux={`sudo systemctl status bee`}
-                      mac={`brew services status swarm-bee`}
-                      />
-                      <li>If your node is running, check your firewall settings to make sure that port 1635 (or your custom specified port) is bound to localhost. If your node is not running try executing the below command to start your bee node</li>
-                      <MuiAlert style={{marginTop:'10px', marginBottom:'10px'}} elevation={6} variant="filled"  severity="error">
-                        Your debug node API should never be completely open to the internet. If you want to connect remotely, make sure your firewall settings are set to only allow specific trusted IP addresses and block all other ports. A simple google search for "what is my ip" will show you your computers public IP address to allow.
-                      </MuiAlert>
-                      <CodeBlockTabs
-                      showLineNumbers
-                      linux={`sudo systemctl start bee`}
-                      mac={`brew services start swarm-bee`}
-                      />
-                      <li>Run the commands to validate your node is running and see the log output.</li>
-                      <CodeBlockTabs
-                      showLineNumbers
-                      linux={`sudo systemctl status bee \njournalctl --lines=100 --follow --unit bee`}
-                      mac={`brew services status swarm-bee \ntail -f /usr/local/var/log/swarm-bee/bee.log`}
-                      />
-                      <li>Lastly, check your nodes configuration settings to validate the debug API is enabled and the Cross Origin Resource Sharing (CORS) setting is configured to allow your host. Config parameter <strong>debug-api-enable</strong> must be set to <strong>true</strong> and <strong>cors-allowed-origins</strong> must be set to your host domain or IP. If edits are made to the configuration run the restart command below for changes to take effect.</li>
-                      <CodeBlockTabs
-                      showLineNumbers
-                      linux={`sudo vi /etc/bee/bee.yaml\nsudo systemctl restart bee`}
-                      mac={`sudo vi /etc/bee/bee.yaml \nbrew services restart swarm-bee`}
-                      />
-                    </ol>
-                    </Typography>
-                  </AccordionDetails>
-                </Accordion>
-              </Typography>
-            :
-            null}
-            </div>
           </div>
     </div>
     )
