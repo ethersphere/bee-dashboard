8d4c51c53c23fa6be5c8d4ae9633de1d1f019424
matmertz25 <40722304+matmertz25@users.noreply.github.com>
2021-03-31 23:07:26 -0400

    chore: split theme into separate file

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,11 @@ import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 
-import { createMuiTheme } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
 import BaseRouter from './routes/routes';
-
+import { lightTheme, darkTheme } from './theme';
 
 declare global {
   interface Window {
@@ -15,62 +14,6 @@ declare global {
     web3: {};
   }
 }
-
-const lightTheme = createMuiTheme({
-  palette: {
-    type: "light",
-    background: {
-      default: '#fafafa',
-    },
-    primary: {
-      main: '#6a6a6a',
-    },
-    secondary: {
-      main: '#333333',
-    },
-  },
-  typography: {
-    fontFamily: [
-      'Work Sans',
-      'Montserrat',
-      'Nunito',
-      'Roboto',
-      '"Helvetica Neue"',
-      'Arial',
-      'sans-serif'
-    ].join(','),
-  }
-});
-
-const darkTheme = createMuiTheme({
-  palette: {
-    type: "dark",
-    background: {
-      default: '#0d1117', //'#111827',
-      paper: '#161b22', //'#1f2937',
-    },
-    primary: {
-      // light: will be calculated from palette.primary.main,
-      main: '#dd7700' //'#3f51b5',
-      // dark: will be calculated from palette.primary.main,
-      // contrastText: will be calculated to contrast with palette.primary.main
-    },
-    secondary: {
-      main: '#1f2937',
-    },
-  },
-  typography: {
-    fontFamily: [
-      'Work Sans',
-      'Montserrat',
-      'Nunito',
-      'Roboto',
-      '"Helvetica Neue"',
-      'Arial',
-      'sans-serif'
-    ].join(','),
-  }
-});
 
 
 function App() {
