d7280a8e04e5205cd8407dc48a448c13f966e1a2
matmertz25 <40722304+matmertz25@users.noreply.github.com>
2021-05-28 12:21:02 -0400

    fix: add serve command in bin

---
+++ serve.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+const path = require('path')
+const handler = require('serve-handler');
+const http = require('http');
+const opener = require('opener')
+
+const serverConfig = {
+  public: path.join(__dirname, 'build'),
+  trailingSlash: false,
+  rewrites: [
+    { source: "**", destination: "/index.html" },
+  ],
+  headers: [
+      {
+        source: "*",
+        headers: [{
+          key: "Cache-Control",
+          value: "max-age=3600"
+        }]
+      }
+  ]
+}
+
+const server = http.createServer((request, response) => {
+
+  return handler(request, response, serverConfig);
+})
+ 
+server.listen(8080, () => {
+  console.log('Starting up Bee Dashboard on address http://localhost:8080')
+  console.log('Hit CTRL-C to stop the server')
+  opener('http://localhost:8080')
+});
