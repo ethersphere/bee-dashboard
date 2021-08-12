4d5ffff69f4c4fcef1bcc80c4f3cf15939add4c9
Cafe137 <aron@aronsoos.com>
2021-08-12 13:18:38 +0200

    style: rename DepositModal to CheckoutModal

--- src/components/CashoutModal.tsx
+++ src/components/CashoutModal.tsx
@@ -1,15 +1,13 @@
-import { ReactElement, useState } from 'react'
+import { CircularProgress, Container } from '@material-ui/core'
 import Button from '@material-ui/core/Button'
 import Dialog from '@material-ui/core/Dialog'
 import DialogActions from '@material-ui/core/DialogActions'
 import DialogContent from '@material-ui/core/DialogContent'
 import DialogContentText from '@material-ui/core/DialogContentText'
 import DialogTitle from '@material-ui/core/DialogTitle'
-import { Container, CircularProgress } from '@material-ui/core'
 import { useSnackbar } from 'notistack'
-
+import { ReactElement, useState } from 'react'
 import { beeDebugApi } from '../services/bee'
-
 import EthereumAddress from './EthereumAddress'
 
 interface Props {
@@ -17,7 +15,7 @@ interface Props {
   uncashedAmount: string
 }
 
-export default function DepositModal({ peerId, uncashedAmount }: Props): ReactElement {
+export default function CheckoutModal({ peerId, uncashedAmount }: Props): ReactElement {
   const [open, setOpen] = useState<boolean>(false)
   const [loadingCashout, setLoadingCashout] = useState<boolean>(false)
   const { enqueueSnackbar } = useSnackbar()
