232e66c31d3a67427581d53bfc6df2ab1f655751
Vojtech Simetka <vojtech@simetka.cz>
2021-10-03 19:27:18 +0200

    refactor: simplified the topology into single component

--- src/components/TopologyStats.tsx
+++ src/components/TopologyStats.tsx
@@ -4,24 +4,24 @@ import { pickThreshold, ThresholdValues } from '../utils/threshold'
 import ExpandableList from './ExpandableList'
 import ExpandableListItem from './ExpandableListItem'
 
-interface RootProps {
+interface Props {
   topology: Topology | null
 }
 
-interface Props extends RootProps {
-  thresholds: ThresholdValues
-}
-
-const TopologyStats = (props: RootProps): ReactElement => {
+const TopologyStats = (props: Props): ReactElement => {
   const thresholds: ThresholdValues = {
     connectedPeers: pickThreshold('connectedPeers', props.topology?.connected || 0),
     population: pickThreshold('population', props.topology?.population || 0),
     depth: pickThreshold('depth', props.topology?.depth || 0),
   }
 
+  const maximumTotalScore = Object.values(thresholds).reduce((sum, item) => sum + item.maximumScore, 0)
+  const actualTotalScore = Object.values(thresholds).reduce((sum, item) => sum + item.score, 0)
+  const percentageText = Math.round((actualTotalScore / maximumTotalScore) * 100) + '%'
+
   return (
     <ExpandableList label="Connectivity" defaultOpen>
-      <Indicator {...props} thresholds={thresholds} />
+      <ExpandableListItem label="Overall Health Indicator" value={percentageText} />
       <ExpandableListItem
         label="Connected Peers"
         value={props.topology?.connected.toString()}
@@ -39,14 +39,6 @@ const TopologyStats = (props: RootProps): ReactElement => {
       />
     </ExpandableList>
   )
-}
-
-const Indicator = ({ thresholds }: Props): ReactElement => {
-  const maximumTotalScore = Object.values(thresholds).reduce((sum, item) => sum + item.maximumScore, 0)
-  const actualTotalScore = Object.values(thresholds).reduce((sum, item) => sum + item.score, 0)
-  const percentageText = Math.round((actualTotalScore / maximumTotalScore) * 100) + '%'
-
-  return <ExpandableListItem label="Overall Health Indicator" value={percentageText} />
 }
 
 export default TopologyStats
