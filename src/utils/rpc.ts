f84952595b76b38a1500e256d06d6bc16aeb6d25
Vojtech Simetka <vojtech@simetka.cz>
2022-03-24 14:49:08 +0100

    feat: display xBZZ balance

--- src/utils/rpc.ts
+++ src/utils/rpc.ts
@@ -1,3 +1,5 @@
+import { Contract, providers } from 'ethers'
+
 const PROVIDER = 'https://dai.poa.network/'
 
 async function eth_getBalance(address: string): Promise<string> {
@@ -18,6 +20,40 @@ async function eth_getBalance(address: string): Promise<string> {
   return json.result
 }
 
+const partialERC20tokenABI = [
+  {
+    constant: true,
+    inputs: [
+      {
+        name: '_owner',
+        type: 'address',
+      },
+    ],
+    name: 'balanceOf',
+    outputs: [
+      {
+        name: 'balance',
+        type: 'uint256',
+      },
+    ],
+    payable: false,
+    type: 'function',
+  },
+]
+
+const provider = new providers.JsonRpcProvider(PROVIDER)
+
+async function eth_getBalanceERC20(
+  address: string,
+  tokenAddress = '0xdbf3ea6f5bee45c02255b2c26a16f300502f68da',
+): Promise<string> {
+  const contract = new Contract(tokenAddress, partialERC20tokenABI, provider)
+  const balance = await contract.balanceOf(address)
+
+  return balance.toString()
+}
+
 export const Rpc = {
   eth_getBalance,
+  eth_getBalanceERC20,
 }
