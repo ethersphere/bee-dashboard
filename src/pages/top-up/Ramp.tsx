eaabcd7707595d017a534dc234edcd896f34344f
Cafe137 <xcafe137@gmail.com>
2022-05-05 14:01:34 +0200

    chore: remove dead code

--- src/pages/top-up/Ramp.tsx
+++ src/pages/top-up/Ramp.tsx
@@ -1,14 +1,5 @@
-import { createStyles, makeStyles } from '@material-ui/core'
 import { RampInstantSDK } from '@ramp-network/ramp-instant-sdk'
 import { ReactElement, useEffect, useRef } from 'react'
-
-const useStyles = makeStyles(() =>
-  createStyles({
-    containerNode: {
-      height: '0px',
-    },
-  }),
-)
 
 interface Props {
   address?: string
@@ -16,7 +7,6 @@ interface Props {
 
 export function Ramp({ address }: Props): ReactElement {
   const ref = useRef(null)
-  const styles = useStyles()
 
   useEffect(() => {
     if (!ref.current || !address) {
@@ -25,8 +15,6 @@ export function Ramp({ address }: Props): ReactElement {
     const widget = new RampInstantSDK({
       hostAppName: 'Swarm',
       hostLogoUrl: 'https://avatars.githubusercontent.com/u/6946989?s=200&v=4',
-      //   variant: 'embedded-mobile',
-      //   containerNode: ref.current,
       fiatValue: '10',
       fiatCurrency: 'EUR',
       userAddress: address,
@@ -39,5 +27,5 @@ export function Ramp({ address }: Props): ReactElement {
     }
   }, [address])
 
-  return <div ref={ref} className={styles.containerNode}></div>
+  return <div></div>
 }
