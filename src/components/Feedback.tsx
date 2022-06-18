eab53ab389875aa6f9d42fdd4ca319de1d12a05d
Adam Uhlíř <adam@uhlir.dev>
2022-06-18 20:26:38 +0200

    feat: sentry available check

--- src/components/Feedback.tsx
+++ src/components/Feedback.tsx
@@ -1,7 +1,8 @@
-import { ReactElement } from 'react'
+import { ReactElement, useState } from 'react'
 import * as Sentry from '@sentry/react'
 import { Link } from '@material-ui/core'
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles'
+import config from '../config'
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -18,6 +19,40 @@ const useStyles = makeStyles((theme: Theme) =>
   }),
 )
 
+/**
+ * Parses Sentry DNS so it could be transformed into API call
+ * Sentry DNS like https://1asfasdf2312asdf3@o132123.ingest.sentry.io/13123123
+ */
+const SENTRY_PARSING_REGEX = /^https:\/\/(?<key>\w+)@(?<sub>\w+)\.ingest\.sentry\.io\/(?<path>\d+)$/gm
+let SENTRY_ACCESS_VERIFIED = false
+
+async function isSentryReachable(): Promise<boolean> {
+  SENTRY_ACCESS_VERIFIED = true
+  const key = config.SENTRY_KEY
+
+  if (!key) {
+    return false
+  }
+
+  const match = SENTRY_PARSING_REGEX.exec(key)
+
+  if (!match) {
+    return false
+  }
+
+  const url = `https://${match.groups?.sub}.ingest.sentry.io/api/${match.groups?.path}/envelope/?sentry_key=${match.groups?.key}`
+
+  try {
+    await fetch(url, { method: 'POST' })
+
+    // Since we got some reply (even though most probably with some error) that means Sentry is reachable ==> lets provide the Feedback form
+    return true
+  } catch (e) {
+    // If an error was thrown than the request was blocked by the browser so Sentry is not accessible to us
+    return false
+  }
+}
+
 function showFeedbackForm(): void {
   const eventId = Sentry.captureMessage('User feedback')
   Sentry.showReportDialog({
@@ -31,11 +66,22 @@ function showFeedbackForm(): void {
 }
 
 export default function Feedback(): ReactElement {
+  const [sentryEnabled, setSentryEnabled] = useState(false)
   const classes = useStyles()
 
-  return (
-    <Link onClick={showFeedbackForm}>
-      <div className={classes.root}>Give us feedback!</div>
-    </Link>
-  )
+  if (!SENTRY_ACCESS_VERIFIED) {
+    isSentryReachable().then(result => {
+      setSentryEnabled(result)
+    })
+  }
+
+  if (sentryEnabled) {
+    return (
+      <Link onClick={showFeedbackForm}>
+        <div className={classes.root}>Give us feedback!</div>
+      </Link>
+    )
+  }
+
+  return <></>
 }
