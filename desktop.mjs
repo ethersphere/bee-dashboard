c601d97ed0f2b900fe57b762c501137c44701818
Adam Uhlíř <adam@uhlir.dev>
2022-06-21 14:39:07 +0200

    chore: new swarm desktop data dir path (#423)

--- desktop.mjs
+++ desktop.mjs
@@ -6,7 +6,7 @@ import open  from 'open'
 import { readFile } from 'node:fs/promises'
 import { join } from 'node:path'
 
-const paths = envPaths('bee-desktop')
+const paths = envPaths('Swarm Desktop', { suffix: '' })
 const apiKey = await readFile(join(paths.data, 'api-key.txt'), {encoding: 'utf-8'})
 const url = `http://localhost:3001/?v=${apiKey}#/`
 
