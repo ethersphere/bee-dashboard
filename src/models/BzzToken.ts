bf25bde828f5b3a5a8984341601dac0904f6fe0e
Cafe137 <xcafe137@gmail.com>
2022-11-08 12:59:49 +0100

    refactor: extract decimal places constants

--- src/models/BzzToken.ts
+++ src/models/BzzToken.ts
@@ -1,12 +1,14 @@
 import { BigNumber } from 'bignumber.js'
 import { Token } from './Token'
 
+const BZZ_DECIMAL_PLACES = 16
+
 export class BzzToken extends Token {
   constructor(value: BigNumber | string | bigint) {
-    super(value, 16)
+    super(value, BZZ_DECIMAL_PLACES)
   }
 
   static fromDecimal(value: BigNumber | string | bigint): BzzToken {
-    return Token.fromDecimal(value, 16)
+    return Token.fromDecimal(value, BZZ_DECIMAL_PLACES)
   }
 }
