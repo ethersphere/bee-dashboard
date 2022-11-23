4e564dd5c08b938c95f07818bc60957a7df4f5bb
Cafe137 <77121044+Cafe137@users.noreply.github.com>
2022-11-23 14:20:55 +0100

    feat: add prerequisite checks before swap (#588)
    * feat: add prerequisite checks before swap
    
    * fix: add missing authentication on desktop config call
    
    * refactor(wip): introduce swap error
    
    * refactor: use wrapWithSwapError
    
    * fix: log originalError instead of error
    
    * fix: show snackbar when error is unexpected

---
+++ src/utils/SwapError.ts
@@ -0,0 +1,21 @@
+export class SwapError extends Error {
+  snackbarMessage: string
+  originalError?: Error
+
+  constructor(snackbarMessage: string, error?: Error) {
+    super(error?.message || snackbarMessage)
+    this.name = 'SwapError'
+    this.originalError = error
+    this.snackbarMessage = snackbarMessage
+  }
+}
+
+export function isSwapError(error: unknown): error is SwapError {
+  return error instanceof Error && error.name === 'SwapError'
+}
+
+export function wrapWithSwapError<T>(promise: Promise<T>, snackbarMessage: string): Promise<T> {
+  return promise.catch((error: Error) => {
+    throw new SwapError(snackbarMessage, error)
+  })
+}
