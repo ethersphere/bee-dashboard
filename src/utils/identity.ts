0841679db98c15cf1345a84b426a5a2d0fae845f
Binyom Binyom Baleba Etobe Binyom <jean-emmanuel.binyom@gmx.fr>
2023-12-13 09:05:26 +0100

    change updateFeed to account for POST update. Add readFeed to retrieve an array of older POST

--- src/utils/identity.ts
+++ src/utils/identity.ts
@@ -1,8 +1,11 @@
 import { BatchId, Bee, BeeDebug, Reference } from '@ethersphere/bee-js'
 import { Wallet } from 'ethers'
 import { uuidV4, waitUntilStampUsable } from '.'
-import { Identity, IdentityType } from '../providers/Feeds'
-
+import { Identity, IdentityType, Post, Context as IdentityContext } from '../providers/Feeds'
+import { FeedUploadOptions } from '@ethersphere/bee-js/dist/types/feed'
+import { ReactElement, useContext, useEffect, useState } from 'react'
+import { Context as FileContext } from '../providers/File'
+import { FileUploadOptions } from '@ethersphere/bee-js'
 export function generateWallet(): Wallet {
   return Wallet.createRandom()
 }
@@ -95,18 +98,95 @@ export async function updateFeed(
   identity: Identity,
   hash: string,
   stamp: string,
+  msg: Post,
   password?: string,
 ): Promise<void> {
   const wallet = await getWalletFromIdentity(identity, password)
+  const [hexTopic, setTopic] = useState('0x0')
+  const { uploadOrigin } = useContext(FileContext)
 
-  if (!identity.feedHash) {
-    identity.feedHash = (await beeApi.createFeedManifest(stamp, 'sequence', '00'.repeat(32), wallet.address)).reference
+  if (uploadOrigin.origin === 'POST') {
+    setTopic(beeApi.makeFeedTopic(identity.topic))
+  } else {
+    setTopic('00'.repeat(32))
   }
 
-  const writer = beeApi.makeFeedWriter('sequence', '00'.repeat(32), wallet.privateKey)
+  if (!identity.feedHash) {
+    identity.feedHash = (await beeApi.createFeedManifest(stamp, 'sequence', hexTopic, wallet.address)).reference
+  }
+
+  const writer = beeApi.makeFeedWriter('sequence', hexTopic, wallet.privateKey)
 
   if (beeDebugApi) {
     await waitUntilStampUsable(stamp as BatchId, beeDebugApi)
   }
-  await writer.upload(stamp, hash as Reference)
+
+  if (uploadOrigin.origin === 'POST') {
+    const postData = JSON.stringify(msg)
+    const fileoptions: FileUploadOptions = {}
+    fileoptions.contentType = 'application/json'
+    const dirHash = await beeApi.uploadFile(stamp, postData, 'file.json', fileoptions)
+
+    if (dirHash) {
+      const result = await writer.upload(stamp, dirHash.reference)
+      console.log('Feed write result hash: ', result)
+    }
+  } else {
+    await writer.upload(stamp, hash as Reference)
+  }
+}
+
+function decimalToHex(d: number, padding: number) {
+  var hex = Number(d).toString(16)
+  padding = typeof padding === 'undefined' || padding === null ? (padding = 2) : padding
+
+  while (hex.length < padding) {
+    hex = '0' + hex
+  }
+
+  return hex
+}
+
+export async function readFeed(
+  beeApi: Bee,
+  beeDebugApi: BeeDebug | null,
+  identity: Identity,
+  password?: string,
+): Promise<Post[]> {
+  const { posts, setPosts } = useContext(IdentityContext)
+  const Posts: Post[] = []
+  const wallet = await getWalletFromIdentity(identity, password)
+  const topic = beeApi.makeFeedTopic(identity.topic)
+  const reader = beeApi.makeFeedWriter('sequence', topic, wallet.address)
+  const options: FeedUploadOptions = {}
+
+  const { reference, feedIndex, feedIndexNext } = await reader.download()
+  const I0 = parseInt(feedIndex, 16)
+
+  for (let i = 0; i <= I0; i++) {
+    options.index = decimalToHex(i, 16).toString()
+    const fv = await reader
+      .download(options)
+      .then(async fv => {
+        const fjson = await beeApi
+          .downloadReadableFile(fv.reference, 'file.json')
+          .then(fjson => {
+            const jdata = JSON.parse(JSON.stringify(fjson.data))
+            try {
+              Posts.push(...[jdata])
+            } catch (e) {
+              console.log('Failed to: ', e)
+            }
+          })
+          .catch(error0 => {
+            console.log(error0)
+          })
+      })
+      .catch(error1 => {
+        console.log(error1)
+      })
+  }
+  setPosts(Posts)
+
+  return Posts
 }
