df49445f1032f1ae5f2c710a5e97312198613d83
Binyom Binyom Baleba Etobe Binyom <jean-emmanuel.binyom@gmx.fr>
2023-12-16 00:15:21 +0100

    Update to display posts for a given feed.

--- src/pages/feeds/FeedSubpage.tsx
+++ src/pages/feeds/FeedSubpage.tsx
@@ -10,36 +10,37 @@ import ExpandableListItem from '../../components/ExpandableListItem'
 import { HistoryHeader } from '../../components/HistoryHeader'
 import { SwarmButton } from '../../components/SwarmButton'
 import { Context as BeeContext } from '../../providers/Bee'
-import { Identity, Context as IdentityContext } from '../../providers/Feeds'
+import { Identity, Post, Context as IdentityContext } from '../../providers/Feeds'
 import { Context as SettingsContext } from '../../providers/Settings'
 import { ROUTES } from '../../routes'
 import { UploadArea } from '../files/UploadArea'
-import { readFeed } from '../../utils/identity'
+
+const msg: Post = {
+  Title: 'PS5',
+  Type: 'Document',
+  Category: 'Loisir',
+  Date: '07/12/2023',
+  Amount: '750€',
+  Provider: 'Sony',
+  Place: 'La Defense',
+  reference: '',
+}
 
 export function FeedSubpage(): ReactElement {
-  const { identities } = useContext(IdentityContext)
-  const { posts } = useContext(IdentityContext)
+  const { identities, isLoadingPosts, setisLoadingPosts, Posts, setPostsList } = useContext(IdentityContext)
   const { uuid } = useParams()
   const { beeApi, beeDebugApi } = useContext(SettingsContext)
   const { status } = useContext(BeeContext)
-
   const navigate = useNavigate()
 
   const [available, setAvailable] = useState(false)
 
   const identity = identities.find(x => x.uuid === uuid)
 
-  if (!beeApi || !beeDebugApi) {
-    return <></>
-  }
-
-  if (identity?.topic === '') {
-  } else {
-    try {
-      const readPostArray = readFeed(beeApi, beeDebugApi, identity as Identity)
-    } catch (error) {
-      return <></>
-    }
+  function onClose() {
+    setPostsList([])
+    setisLoadingPosts(true)
+    navigate(ROUTES.ACCOUNT_FEEDS)
   }
 
   useEffect(() => {
@@ -60,13 +61,9 @@ export function FeedSubpage(): ReactElement {
     return <></>
   }
 
-  function onClose() {
-    navigate(ROUTES.ACCOUNT_FEEDS)
-  }
-
   return (
     <div>
-      <HistoryHeader>{`${identity.name} Website`}</HistoryHeader>
+      <HistoryHeader>{`${identity.topic}`}</HistoryHeader>
       {identity.topic === '' ? (
         <UploadArea showHelp={false} uploadOrigin={{ origin: 'FEED', uuid }} />
       ) : (
@@ -93,21 +90,23 @@ export function FeedSubpage(): ReactElement {
           </DocumentationText>
         </Box>
       )}
-      // Posts form
-      {posts.map((x, i) => (
-        <ExpandableList key={i} label={`${x.Title} Website`} defaultOpen>
-          <Box mb={0.5}>
-            <ExpandableList label={x.Title} level={1}>
-              <ExpandableListItem label="Post Type" value={x.Type} />
-              <ExpandableListItem label="Post Date" value={x.Date} />
-              <ExpandableListItem label="Post Amount" value={x.Amount} />
-              <ExpandableListItem label="Post Provider" value={x.Provider} />
-              <ExpandableListItem label="Post Place" value={x.Place} />
-              /* Add a clickable bzz link to a bill scan here */
-            </ExpandableList>
-          </Box>
-        </ExpandableList>
-      ))}
+      <ExpandableList label={`List of posts to - ${identity.topic}`} defaultOpen>
+        <Box mb={0.5}>
+          {isLoadingPosts ? (
+            <ExpandableList label={`...Loading List of posts to - ${identity.topic}`} defaultOpen />
+          ) : (
+            Posts.map((x, i) => (
+              <ExpandableList key={i} label={x.Title} level={1}>
+                <ExpandableListItem label="Post Type" value={x.Type} />
+                <ExpandableListItem label="Post Date" value={x.Date} />
+                <ExpandableListItem label="Post Amount" value={x.Amount} />
+                <ExpandableListItem label="Post Provider" value={x.Provider} />
+                <ExpandableListItem label="Post Place" value={x.Place} />
+              </ExpandableList>
+            ))
+          )}
+        </Box>
+      </ExpandableList>
       <ExpandableListItemActions>
         <SwarmButton iconType={X} onClick={onClose} cancel>
           Close
