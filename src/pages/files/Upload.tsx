9576ed1c725e3d54fc7926006d5ea81d3cfb41a4
Levente Kiss <levente.kiss@solarpunk.bzz>
2023-12-18 09:59:21 +0100

    refactor: use the documentation text comp (#605)

--- src/pages/files/Upload.tsx
+++ src/pages/files/Upload.tsx
@@ -207,7 +207,11 @@ export function Upload(): ReactElement {
       {step === 2 && stamp && (
         <>
           <StampPreview stamp={stamp} />
-          <Box mb={4}>Please do not close the application until your file is uploaded to your local node!</Box>
+          <Box mb={4}>
+            <DocumentationText>
+              Please do not close the application until your file is uploaded to your local node!
+            </DocumentationText>
+          </Box>
         </>
       )}
       <UploadActionBar
