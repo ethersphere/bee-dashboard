5abfbb1b255128200d879a9a83b500f3f9cdea3d
Levente Kiss <levente.kiss@solarpunk.bzz>
2023-12-18 12:41:50 +0100

    refactor: optimize sync check (#605)

--- src/pages/files/AssetSyncing.tsx
+++ src/pages/files/AssetSyncing.tsx
@@ -1,6 +1,6 @@
 import { Context as SettingsContext } from '../../providers/Settings'
 import { Box } from '@material-ui/core'
-import { ReactElement, useCallback, useContext, useEffect, useRef, useState } from 'react'
+import { ReactElement, useContext, useEffect, useRef, useState } from 'react'
 import { DocumentationText } from '../../components/DocumentationText'
 import { LinearProgressWithLabel } from '../../components/ProgressBar'
 
@@ -12,9 +12,10 @@ export function AssetSyncing({ reference }: Props): ReactElement {
   const { beeApi } = useContext(SettingsContext)
 
   const syncTimer = useRef<NodeJS.Timer>()
+  const [isRetrieveChecking, setIsRetrieveChecking] = useState<boolean>(false)
   const [syncProgress, setSyncProgress] = useState<number>(0)
 
-  const syncCheck = useCallback(async () => {
+  const syncCheck = async () => {
     if (!beeApi) {
       return
     }
@@ -25,18 +26,8 @@ export function AssetSyncing({ reference }: Props): ReactElement {
     if (tag) {
       const progress = ((tag.seen + tag.synced) / tag.split) * 100
       setSyncProgress(progress)
-
-      // Check availablity from 70%
-      // There are occasions when it shows that the content is not synced but already available
-      if (progress > 70 && progress < 100) {
-        const isRetriavable = await beeApi.isReferenceRetrievable(reference)
-
-        if (isRetriavable) {
-          setSyncProgress(100)
-        }
-      }
     }
-  }, [beeApi, reference])
+  }
 
   useEffect(() => {
     syncTimer.current = setInterval(syncCheck, 2000)
@@ -55,13 +46,35 @@ export function AssetSyncing({ reference }: Props): ReactElement {
     }
   }, [syncProgress])
 
+  useEffect(() => {
+    /*   
+          There are instances when it seems that the content isn't synchronized, despite being already available.
+          To ensure it's not due to invalid synchronization data,
+          verify availability from at least 70% using one of the stewardship endpoints.
+          
+          TODO: is 70 a good number?
+    */
+    if (beeApi && !isRetrieveChecking && syncProgress > 10 && syncProgress < 100) {
+      // It's a long running task make sure only one run occurs at a time.
+      setIsRetrieveChecking(true)
+
+      beeApi.isReferenceRetrievable(reference).then(isRetriavable => {
+        if (isRetriavable) {
+          setSyncProgress(100)
+        }
+
+        setIsRetrieveChecking(false)
+      })
+    }
+  }, [syncProgress, isRetrieveChecking, beeApi, reference])
+
   return (
     <>
       <Box mb={2}>
         <DocumentationText>
           Files are not immediately accessible on the Swarm network. Please wait until your upload is synced to the
           network.{' '}
-          <a href="https://docs.ethswarm.org/docs/develop/access-the-swarm/syncing">Learn more about syncing</a>
+          <a href="https://docs.ethswarm.org/docs/develop/access-the-swarm/syncing">Learn more about syncing</a>.
         </DocumentationText>
       </Box>
       <Box mb={4}>
