b95bf97eb489e9bb4e03b9d2369b5e61be55dc01
Levente Kiss <levente.kiss@solarpunk.bzz>
2025-02-12 10:00:42 +0100

    fix: make the media check more strict

--- src/utils/video.ts
+++ src/utils/video.ts
@@ -2,7 +2,7 @@ export function isSupportedVideoType(type: string) {
   const video = document.createElement('video')
 
   const result = video.canPlayType(type)
-  const isDefinitelySupported = result && result !== 'maybe' && result !== 'probably'
+  const isDefinitelySupported = result && result !== 'maybe'
 
   return Boolean(isDefinitelySupported)
 }
