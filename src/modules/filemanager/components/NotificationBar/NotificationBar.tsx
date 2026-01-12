e0e471452eaaa1926cc9e636eb82c040d2cea694
Bálint Ujvári <58116288+bosi95@users.noreply.github.com>
2026-01-12 15:13:36 +0100

    fix: listen to drive upgrade end in notificationbar (#169)

--- src/modules/filemanager/components/NotificationBar/NotificationBar.tsx
+++ src/modules/filemanager/components/NotificationBar/NotificationBar.tsx
@@ -7,9 +7,17 @@ import { Context as SettingsContext } from '../../../../providers/Settings'
 import { PostageBatch } from '@ethersphere/bee-js'
 import { Context as FMContext } from '../../../../providers/FileManager'
 import { DriveInfo } from '@solarpunkltd/file-manager-lib'
+import { FILE_MANAGER_EVENTS } from '../../constants/common'
 
 const NUMBER_OF_DAYS_WARNING = 7
 const DAYS_TO_MILLISECONDS_MULTIPLIER = 24 * 60 * 60 * 1000
+
+const isExpiring = (s: PostageBatch): boolean => {
+  return (
+    s.duration &&
+    s.duration.toEndDate().getTime() <= Date.now() + NUMBER_OF_DAYS_WARNING * DAYS_TO_MILLISECONDS_MULTIPLIER
+  )
+}
 
 interface NotificationBarProps {
   setErrorMessage?: (error: string) => void
@@ -38,12 +46,7 @@ export function NotificationBar({ setErrorMessage }: NotificationBarProps): Reac
           (adminDrive?.batchId.toString() === stamp.batchID.toString() ? adminDrive : null)
 
         if (matchingDrive) {
-          const isExpiring =
-            stamp.duration &&
-            stamp.duration.toEndDate().getTime() <=
-              Date.now() + NUMBER_OF_DAYS_WARNING * DAYS_TO_MILLISECONDS_MULTIPLIER
-
-          if (isExpiring) {
+          if (isExpiring(stamp)) {
             expiringStamps.push(stamp)
             expiringDrives.push(matchingDrive)
           }
@@ -61,7 +64,35 @@ export function NotificationBar({ setErrorMessage }: NotificationBarProps): Reac
     return () => {
       isMounted = false
     }
-  }, [beeApi, drives, adminDrive])
+  }, [beeApi, drives, adminDrive, setErrorMessage])
+
+  useEffect(() => {
+    const onDriveUpgradeEnd = (e: Event) => {
+      const { driveId, success, updatedStamp } = (e as CustomEvent).detail || {}
+
+      if (success && updatedStamp && driveId) {
+        if (!isExpiring(updatedStamp)) {
+          setStampsToExpire(prev => {
+            const stampIx = prev.findIndex(s => s.batchID.toString() === updatedStamp.batchID.toString())
+
+            return stampIx !== -1 ? prev.filter((_, i) => i !== stampIx) : prev
+          })
+
+          setDrivesToExpire(prev => {
+            const driveIx = prev.findIndex(d => d.id.toString() === driveId)
+
+            return driveIx !== -1 ? prev.filter((_, i) => i !== driveIx) : prev
+          })
+        }
+      }
+    }
+
+    window.addEventListener(FILE_MANAGER_EVENTS.DRIVE_UPGRADE_END, onDriveUpgradeEnd as EventListener)
+
+    return () => {
+      window.removeEventListener(FILE_MANAGER_EVENTS.DRIVE_UPGRADE_END, onDriveUpgradeEnd as EventListener)
+    }
+  }, [])
 
   if (!showExpiration) return null
 
