519399cff43744c2a18b75c5023e72f5d52f265c
Nandor Komlodi <nandor.komlodi@gmail.com>
2026-01-28 15:01:39 +0100

    fix: [SPDV-828] use react router link instead of href

--- src/pages/not-found/PageNotFound.tsx
+++ src/pages/not-found/PageNotFound.tsx
@@ -1,13 +1,15 @@
 import { ReactElement } from 'react'
 import { HistoryHeader } from '../../components/HistoryHeader'
 import { Typography } from '@material-ui/core'
+import { Link } from 'react-router-dom'
+import { ROUTES } from 'src/routes'
 
 export default function PageNotFound(): ReactElement {
   return (
     <div>
       <HistoryHeader>Page not found</HistoryHeader>
       <Typography>
-        The given url is invalid. Please go back or <a href="/">navigate to Home screen.</a>
+        The given url is invalid. Please go back or <Link to={ROUTES.INFO}>navigate to Home screen.</Link>
       </Typography>
     </div>
   )
