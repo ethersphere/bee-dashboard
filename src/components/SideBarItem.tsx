fa8a26e80d83d5c794a9d572cf716f0cb484e642
rolandlor <33499567+rolandlor@users.noreply.github.com>
2026-03-13 13:15:26 +0100

    feat: spdv-995 (#225)

--- src/components/SideBarItem.tsx
+++ src/components/SideBarItem.tsx
@@ -1,4 +1,4 @@
-import { ListItemButton, ListItemIcon, ListItemText } from '@mui/material'
+import { ListItemButton, ListItemIcon, ListItemText, Tooltip } from '@mui/material'
 import type { ReactElement, ReactNode } from 'react'
 import { matchPath, useLocation } from 'react-router-dom'
 import { makeStyles } from 'tss-react/mui'
@@ -24,14 +24,24 @@ const useItemStyles = makeStyles()(theme => ({
       },
     },
   },
+  rootCollapsed: {
+    justifyContent: 'center',
+    paddingLeft: theme.spacing(1),
+    paddingRight: theme.spacing(1),
+  },
 }))
 
 const useStyles = makeStyles()(theme => ({
   icon: {
     color: 'inherit',
+    minWidth: 0,
   },
   activeIcon: {
     color: theme.palette.primary.main,
+    minWidth: 0,
+  },
+  label: {
+    marginLeft: theme.spacing(2),
   },
 }))
 
@@ -41,9 +51,17 @@ interface Props {
   path?: string
   label: ReactNode
   pathMatcherSubstring?: string
+  isCollapsed?: boolean
 }
 
-export default function SideBarItem({ iconStart, iconEnd, path, label, pathMatcherSubstring }: Props): ReactElement {
+export default function SideBarItem({
+  iconStart,
+  iconEnd,
+  path,
+  label,
+  pathMatcherSubstring,
+  isCollapsed,
+}: Props): ReactElement {
   const { classes } = useStyles()
   const { classes: itemClasses } = useItemStyles()
   const location = useLocation()
@@ -52,10 +70,18 @@ export default function SideBarItem({ iconStart, iconEnd, path, label, pathMatch
     : Boolean(path && matchPath(location.pathname, path))
 
   return (
-    <ListItemButton className={itemClasses.root} selected={isSelected} disableRipple>
-      <ListItemIcon className={isSelected ? classes.activeIcon : classes.icon}>{iconStart}</ListItemIcon>
-      <ListItemText primary={label} />
-      <ListItemIcon className={isSelected ? classes.activeIcon : classes.icon}>{iconEnd}</ListItemIcon>
-    </ListItemButton>
+    <Tooltip title={isCollapsed ? label : ''} placement="right">
+      <ListItemButton
+        className={`${itemClasses.root} ${isCollapsed ? itemClasses.rootCollapsed : ''}`}
+        selected={isSelected}
+        disableRipple
+      >
+        <ListItemIcon className={isSelected ? classes.activeIcon : classes.icon}>{iconStart}</ListItemIcon>
+        {!isCollapsed && <ListItemText primary={label} className={classes.label} />}
+        {!isCollapsed && iconEnd && (
+          <ListItemIcon className={isSelected ? classes.activeIcon : classes.icon}>{iconEnd}</ListItemIcon>
+        )}
+      </ListItemButton>
+    </Tooltip>
   )
 }
