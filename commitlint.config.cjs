d0922eb9a4f7db3b7d7c29309e23795e20a60389
Balint Ujvari <balint.ujvari@solarpunk.buzz>
2026-03-20 15:16:03 +0100

    chore: set max commit header length

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -2,5 +2,6 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'body-max-line-length': [1, 'always', 120],
+    'header-max-length': [2, 'always', 160],
   },
 }
