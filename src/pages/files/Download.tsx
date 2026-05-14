89448b59f557f775a092ae3b0e848ce1a0303a5d
rolandlor <33499567+rolandlor@users.noreply.github.com>
2026-05-14 12:28:02 +0200

    fix: raw bytes hash download (#252)
    * fix: raw bytes hash download
    
    * refactor: spdv-595
    
    * refactor: trycatch helper for download and share
    
    ---------
    
    Co-authored-by: Balint Ujvari <balint.ujvari@solarpunk.buzz>

--- src/pages/files/Download.tsx
+++ src/pages/files/Download.tsx
@@ -11,6 +11,7 @@ import { Context as FileContext, defaultUploadOrigin } from '../../providers/Fil
 import { Context as SettingsContext } from '../../providers/Settings'
 import { ROUTES } from '../../routes'
 import { recognizeEnsOrSwarmHash, regexpEns } from '../../utils'
+import { tryCatch } from '../../utils/common'
 import { determineHistoryName, LocalStorageKeys, putHistory } from '../../utils/localStorage'
 import { loadManifest } from '../../utils/manifest'
 
@@ -46,29 +47,43 @@ export function Download(): ReactElement {
     setLoading(true)
 
     try {
-      const manifest = await loadManifest(beeApi, identifier)
-      const rootMetadata = manifest.getDocsMetadata()
+      const [manifest] = await tryCatch(() => loadManifest(beeApi, identifier))
 
-      putHistory(
-        LocalStorageKeys.downloadHistory,
-        identifier,
-        determineHistoryName(identifier, rootMetadata.indexDocument),
-      )
-      setUploadOrigin(defaultUploadOrigin)
-      navigate(ROUTES.HASH.replace(':hash', identifier))
-    } catch (error: unknown) {
-      let message = typeof error === 'object' && error !== null && Reflect.get(error, 'message')
+      if (manifest) {
+        const rootMetadata = manifest.getDocsMetadata()
+        putHistory(
+          LocalStorageKeys.downloadHistory,
+          identifier,
+          determineHistoryName(identifier, rootMetadata.indexDocument),
+        )
+        setUploadOrigin(defaultUploadOrigin)
+        navigate(ROUTES.HASH.replace(':hash', identifier))
 
-      if (message.includes('path address not found')) {
-        message = 'The specified hash does not have an index document set.'
+        return
       }
 
-      if (message.includes('Not Found: Not Found')) {
-        message = 'The specified hash was not found.'
+      const [, rawBytesError] = await tryCatch(() => beeApi.downloadData(identifier))
+
+      if (!rawBytesError) {
+        putHistory(LocalStorageKeys.downloadHistory, identifier, identifier)
+        setUploadOrigin(defaultUploadOrigin)
+        navigate(ROUTES.HASH.replace(':hash', identifier))
+      } else {
+        const rawMessage =
+          typeof rawBytesError === 'object' && rawBytesError !== null && Reflect.get(rawBytesError, 'message')
+        let message = typeof rawMessage === 'string' ? rawMessage : undefined
+
+        if (message?.includes('path address not found')) {
+          message = 'The specified hash does not have an index document set.'
+        }
+
+        if (message?.includes('Not Found: Not Found')) {
+          message = 'The specified hash was not found.'
+        }
+        // eslint-disable-next-line no-console
+        console.error(rawBytesError)
+        enqueueSnackbar(<span>Error: {message || 'Unknown'}</span>, { variant: 'error' })
       }
-      // eslint-disable-next-line no-console
-      console.error(error)
-      enqueueSnackbar(<span>Error: {message || 'Unknown'}</span>, { variant: 'error' })
     } finally {
       setLoading(false)
     }
