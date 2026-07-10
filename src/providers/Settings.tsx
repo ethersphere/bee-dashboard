1e627028acafd14f2cfd2313d433a879bd094c86
Hegyes István <96654261+Pityesz1332@users.noreply.github.com>
2026-07-10 15:32:31 +0200

    feat: make ENS resolver URL editable from UI (#283)
    * feat: make ENS resolver URL editable from UI
    
    * fix: keeping ENS resolver UI state in sync

--- src/providers/Settings.tsx
+++ src/providers/Settings.tsx
@@ -24,6 +24,7 @@ interface ContextInterface {
   ensResolver: string | null
   setApiUrl: (url: string) => void
   setAndPersistJsonRpcProvider: (url: string) => void
+  setEnsResolver: (url: string) => void
   isLoading: boolean
   error: Error | null
 }
@@ -43,6 +44,7 @@ const initialValues: ContextInterface = {
   dataDir: null,
   configFile: null,
   ensResolver: null,
+  setEnsResolver: () => {},
   isLoading: true,
   error: null,
 }
@@ -76,6 +78,7 @@ export function Provider({ children, ...propsSettings }: Props): ReactElement {
   const [desktopApiKey, setDesktopApiKey] = useState<string>(initialValues.desktopApiKey)
   const [rpcProviderUrl, setRpcProviderUrl] = useState(propsProviderUrl)
   const [rpcProvider, setRpcProvider] = useState(newGnosisProvider(propsProviderUrl))
+  const [ensResolver, setEnsResolverState] = useState<string | null>(initialValues.ensResolver)
 
   const { config, isLoading, error } = useGetBeeConfig(desktopUrl)
 
@@ -114,6 +117,12 @@ export function Provider({ children, ...propsSettings }: Props): ReactElement {
     setRpcProvider(newGnosisProvider(daemonRpcUrl))
   }, [isDesktop, config])
 
+  useEffect(() => {
+    if (!isDesktop || !config?.['resolver-options']) return
+
+    setEnsResolverState(config['resolver-options'])
+  }, [isDesktop, config])
+
   const updateApiUrl = useCallback((url: string) => {
     const userProvidedUrl = makeHttpUrl(url)
 
@@ -132,6 +141,10 @@ export function Provider({ children, ...propsSettings }: Props): ReactElement {
     setRpcProvider(newGnosisProvider(providerUrl))
   }, [])
 
+  const setEnsResolver = useCallback((url: string) => {
+    setEnsResolverState(url)
+  }, [])
+
   const contextValue = useMemo(
     () => ({
       apiUrl,
@@ -147,8 +160,9 @@ export function Provider({ children, ...propsSettings }: Props): ReactElement {
       cors: config?.['cors-allowed-origins'] ?? null,
       dataDir: config?.['data-dir'] ?? null,
       configFile: config?.['config-file-path'] ?? null,
-      ensResolver: config?.['resolver-options'] ?? null,
+      ensResolver,
       setAndPersistJsonRpcProvider,
+      setEnsResolver,
       isLoading,
       error,
     }),
@@ -164,7 +178,9 @@ export function Provider({ children, ...propsSettings }: Props): ReactElement {
       rpcProvider,
       rpcProviderUrl,
       config,
+      ensResolver,
       setAndPersistJsonRpcProvider,
+      setEnsResolver,
       isLoading,
       error,
     ],
